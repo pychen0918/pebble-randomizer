87d4cd59e317bf6b1b31100eccafa7a1e69aab9f
pychen <pychen0918@gmail.com>
2015-08-05 23:08:21 +0800

    Add result window and display static text

--- src/pebble-randomizer.c
+++ src/pebble-randomizer.c
@@ -13,6 +13,7 @@ static MenuLayer *s_menu_layer;
 
 // The random pick result
 static Window *s_result_window;
+static TextLayer *s_result_layer;
 
 // The list menu for all candidate
 static Window *s_list_window;
@@ -32,16 +33,25 @@ static void draw_row_handler(GContext *ctx, const Layer *cell_layer, MenuIndex *
 }
 
 static void select_callback(struct MenuLayer *menu_layer, MenuIndex *cell_index, void *callback_context){
+
 	APP_LOG(APP_LOG_LEVEL_INFO, "Select Click");
+	switch(cell_index->row){
+		case 0:
+			window_stack_push(s_result_window, true);
+			break;
+		default:
+			break;
+	}
 }
 
 static void menu_window_load(Window *window) {
 	// Create Window's child Layers here
 	Layer *window_layer = window_get_root_layer(window);
 	GRect bounds = layer_get_bounds(window_layer);
-	s_menu_layer = menu_layer_create(bounds);
 
 	APP_LOG(APP_LOG_LEVEL_INFO, "Menu load");
+
+	s_menu_layer = menu_layer_create(bounds);
 	menu_layer_set_callbacks(s_menu_layer, NULL, (MenuLayerCallbacks){
 		.get_num_rows = get_num_rows_callback,
 		.draw_row = draw_row_handler,
@@ -52,8 +62,27 @@ static void menu_window_load(Window *window) {
 }
 
 static void menu_window_unload(Window *window) {
-	// Destroy Window's child 
 	menu_layer_destroy(s_menu_layer);
+}
+
+static void result_window_load(Window *window) {
+	static char result[16];
+	Layer *window_layer = window_get_root_layer(window);
+	GRect bounds = layer_get_bounds(window_layer);
+	
+	APP_LOG(APP_LOG_LEVEL_INFO, "Result load");
+
+	s_result_layer = text_layer_create(bounds);
+	text_layer_set_font(s_result_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+	text_layer_set_text_alignment(s_result_layer, GTextAlignmentCenter);
+	text_layer_set_background_color(s_result_layer, GColorClear);
+	text_layer_set_text_color(s_result_layer, GColorBlack);
+	text_layer_set_text(s_result_layer, "Result");
+	layer_add_child(window_layer, text_layer_get_layer(s_result_layer));
+}
+
+static void result_window_unload(Window *window) {
+	text_layer_destroy(s_result_layer);
 }
 
 static void init(){
@@ -63,8 +92,17 @@ static void init(){
 		.load = menu_window_load,
 		.unload = menu_window_unload,
 	});
+
+	// Create result window
+	s_result_window = window_create();
+	window_set_window_handlers(s_result_window, (WindowHandlers){
+		.load = result_window_load,
+		.unload = result_window_unload,
+	});
+
 	window_stack_push(s_menu_window, true);
 }
+
 
 static void deinit(){
 	// Destoy main window
