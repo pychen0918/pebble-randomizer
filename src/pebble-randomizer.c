1167f5652bca2730ff760ffa87700913271e5434
pychen <pychen0918@gmail.com>
2015-08-25 01:52:40 +0800

    Layout adjust in result window

--- src/pebble-randomizer.c
+++ src/pebble-randomizer.c
@@ -164,6 +164,7 @@ static Window *s_result_window;
 static TextLayer *s_result_title_text_layer;
 static TextLayer *s_result_sub_text_layer;
 static ActionBarLayer *s_result_action_bar_layer;
+static ScrollLayer *s_result_scroll_layer;
 
 // The list menu for all candidate
 static Window *s_list_window;
@@ -551,8 +552,9 @@ static void result_window_load(Window *window) {
 	uint8_t status;
 	int text_layer_width;
 	bool create_action_bar = false; // create action bar only when valid result is displayed
-	GSize max_size;
+	GSize max_size, sub_max_size;
 	int title_height;
+	TextLayer *temp;
 	
 	APP_LOG(APP_LOG_LEVEL_DEBUG, "Result window load");
 	status = s_search_result.query_status;
@@ -590,30 +592,50 @@ static void result_window_load(Window *window) {
 		text_layer_width = bounds.size.w - ACTION_BAR_WIDTH - 3;
 		s_result_action_bar_layer = action_bar_layer_create();
 		action_bar_layer_add_to_window(s_result_action_bar_layer, window);
-		action_bar_layer_set_click_config_provider(s_result_action_bar_layer, (ClickConfigProvider)result_click_config_provider);
-
 		action_bar_layer_set_icon(s_result_action_bar_layer, BUTTON_ID_SELECT, s_icon_agenda_bitmap);
 	}
 	else{
 		text_layer_width = bounds.size.w;
 	}
+	// compute require height for sub-title
+	temp = text_layer_create(GRect(bounds.origin.x, bounds.origin.y, text_layer_width, bounds.size.h));
+	text_layer_set_font(temp, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+	text_layer_set_text(temp, sub_text);
+	sub_max_size = text_layer_get_content_size(temp);
+	text_layer_destroy(temp);
+
 	// title part
 	s_result_title_text_layer = text_layer_create(GRect(bounds.origin.x, bounds.origin.y, text_layer_width, 2000));
 	text_layer_set_font(s_result_title_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
 	text_layer_set_text_alignment(s_result_title_text_layer, GTextAlignmentLeft);
 	text_layer_set_background_color(s_result_title_text_layer, GColorClear);
 	text_layer_set_text_color(s_result_title_text_layer, GColorBlack);
+	text_layer_set_overflow_mode(s_result_title_text_layer, GTextOverflowModeWordWrap);
 	text_layer_set_text(s_result_title_text_layer, title_text);
 	max_size = text_layer_get_content_size(s_result_title_text_layer);
-	if(max_size.h < (bounds.size.h/2))
-		title_height = (bounds.size.h/2);
+
+	// adjust title height
+	if(max_size.h > (bounds.size.h - sub_max_size.h))
+		title_height = (bounds.size.h - sub_max_size.h);  // title is very long: leave some space for sub-title.
+	else if(max_size.h < (bounds.size.h/2))
+		title_height = (bounds.size.h/2);  // title is short: make it half part
 	else
-		title_height = max_size.h+10;  // +10 for spacing
-	layer_add_child(window_layer, text_layer_get_layer(s_result_title_text_layer));
+		title_height = max_size.h;  // title is long, but not very long: use current size
+	text_layer_set_size(s_result_title_text_layer, max_size);
+
+	// create scroll layer
+	s_result_scroll_layer = scroll_layer_create(GRect(bounds.origin.x, bounds.origin.y, text_layer_width, title_height));
+	scroll_layer_set_click_config_onto_window(s_result_scroll_layer, window);
+	scroll_layer_set_callbacks(s_result_scroll_layer, (ScrollLayerCallbacks){.click_config_provider=result_click_config_provider});
+	scroll_layer_set_content_size(s_result_scroll_layer, GSize(text_layer_width, max_size.h));
+	scroll_layer_add_child(s_result_scroll_layer, text_layer_get_layer(s_result_title_text_layer));
+	layer_add_child(window_layer, scroll_layer_get_layer(s_result_scroll_layer));
+
 	// sub title part
 	s_result_sub_text_layer = text_layer_create(GRect(bounds.origin.x, bounds.origin.y+title_height, text_layer_width, (bounds.size.h-title_height)));
-	text_layer_set_font(s_result_sub_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+	text_layer_set_font(s_result_sub_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
 	text_layer_set_text_alignment(s_result_sub_text_layer, GTextAlignmentLeft);
+	text_layer_set_overflow_mode(s_result_sub_text_layer, GTextOverflowModeFill);
 	text_layer_set_background_color(s_result_sub_text_layer, GColorClear);
 	text_layer_set_text_color(s_result_sub_text_layer, GColorBlack);
 	text_layer_set_text(s_result_sub_text_layer, sub_text);
@@ -626,6 +648,7 @@ static void result_window_unload(Window *window) {
 	text_layer_destroy(s_result_title_text_layer);
 	text_layer_destroy(s_result_sub_text_layer);
 	action_bar_layer_destroy(s_result_action_bar_layer);
+	scroll_layer_destroy(s_result_scroll_layer);
 	if(s_icon_agenda_bitmap != NULL){
 		gbitmap_destroy(s_icon_agenda_bitmap);
 		s_icon_agenda_bitmap = NULL;
